c3b7aae4d366bae9c1688c7cafad86ea25e6435a
Tony <tonyhx2021@gmail.com>
2026-04-04 17:56:54 -0400

    fix(image): skip basePath prefix for absolute URLs

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,7 +3,7 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH
 
 const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
+  <NextImage src={typeof src === 'string' && src.startsWith('http') ? src : `${basePath || ''}${src}`} {...rest} />
 )
 
 export default Image

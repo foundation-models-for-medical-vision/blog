ca372a20e1d4ba416311ccee31c56e1364426685
Tony <tonyhx2021@gmail.com>
2026-04-04 18:34:40 -0400

    style(image): fix prettier formatting

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,7 +3,10 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH
 
 const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={typeof src === 'string' && src.startsWith('http') ? src : `${basePath || ''}${src}`} {...rest} />
+  <NextImage
+    src={typeof src === 'string' && src.startsWith('http') ? src : `${basePath || ''}${src}`}
+    {...rest}
+  />
 )
 
 export default Image
